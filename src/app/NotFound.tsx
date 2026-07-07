88d72ec09fc0f3252f465d6cc95626b56cde6c56
YsmaelM <ysmaelmarcano21@gmail.com>
2026-07-07 16:47:16 -0400

    styling 404 page NotFound.tsx

--- src/app/NotFound.tsx
+++ src/app/NotFound.tsx
@@ -1,12 +1,12 @@
 // src/app/NotFound.tsx
-import { Link } from "react-router-dom";
+
 
 export default function NotFound() {
   return (
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <h1>404 - Página No Encontrada</h1>
       <p>Lo sentimos, la página que buscas no existe.</p>
-      <Link to="/">Volver al Inicio</Link>
+      <input type="button" value="Volver al Inicio" onClick={() => window.location.href = "/"} className="mt-5 cursor-pointer rounded-lg p-2 text-white bg-primary-500" />
     </div>
   );
 }
