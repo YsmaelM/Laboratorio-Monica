91af6b16779b551849ff09486b965940598d9289
YsmaelM <ysmaelmarcano21@gmail.com>
2026-07-10 02:38:07 -0400

    fixed wrap issue 2

--- src/features/reports/styles/pdfStyles.ts
+++ src/features/reports/styles/pdfStyles.ts
@@ -39,8 +39,8 @@ export const s = StyleSheet.create({
     fontFamily: "Inter",
     fontSize: 9,
     color: COLORS.text,
-    paddingTop: 165,
-    paddingBottom: 120,
+    paddingTop: 150,
+    paddingBottom: 100,
     paddingHorizontal: 40,
   },
 
